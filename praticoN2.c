e389da68d1b37cd99b9fe65611e96809cd16a7d4
danielvictorsg <danielvictor.dszg@gmail.com>
2024-04-26 19:39:52 -0300

    representar poligono e area triangulo

--- praticoN2.c
+++ praticoN2.c
@@ -5,3 +5,13 @@ typedef struct {
     float X;
     float Y;
 } Ponto;
+
+// Struct para representar um polígono
+typedef struct {
+    int numVertices;
+    Ponto *vertices;
+} Poligono;
+
+float AreaTriangulo(Ponto A, Ponto B, Ponto C) {
+    return 0.5 * ((A.X * (B.Y - C.Y)) + (B.X * (C.Y - A.Y)) + (C.X * (A.Y - B.Y)));
+}
